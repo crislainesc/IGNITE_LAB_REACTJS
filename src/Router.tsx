0ddd21753e6dadd782a57c3b72b9f6a0d49a661d
Crislâine Santos <criis.santos@outlook.com>
2022-06-25 06:41:55 -0300

    ✨ class 4: registration via GraphQL

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,11 @@
 import {Routes, Route} from 'react-router-dom';
 
-import {Event} from '@pages';
+import {Event, Subscribe} from '@pages';
 
 export default function Router() {
     return (
         <Routes>
-            <Route path="/" element={<h1>Home</h1>} />
+            <Route path="/" element={<Subscribe />} />
             <Route path="/event" element={<Event />} />
             <Route path="/event/lesson/:slug" element={<Event />} />
         </Routes>
